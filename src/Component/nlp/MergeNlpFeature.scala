da1ad1231210fcce169e2c25849b89d595cc4f06
shcup@126.com <rec1223a>
2017-01-03 12:37:24 +0800

    add by lujing 0103

--- src/Component/nlp/MergeNlpFeature.scala
+++ src/Component/nlp/MergeNlpFeature.scala
@@ -1,10 +1,13 @@
 package Component.nlp
 
+import java.lang.Double
+import java.util
+
 import ldacore.CalLDA
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import pipeline.CompositeDoc
-
+import collection.JavaConverters._
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ArrayBuffer
 /**
@@ -16,45 +19,53 @@ object MergeNlpFeature {
     val sc=rdd.sparkContext
     val feature=rdd.map(e=>(e,e.feature_list)).filter(_._2!=null).filter(_._2.length>0)
         .mapValues(_.toList.map(ee=>(ee.name,ee.weight.toDouble)))
-    val result=CalLDA.getLDAModel[CompositeDoc](sc,feature,100)._1
+          .map(e=>(e._1.media_doc_info.id,e._2))
+    val result=CalLDA.getLDAModel[String](sc,feature,100)._1
+      .map(e=>(e._1+"\t"+e._2.toArray.mkString(" ")))
+
     if(outputPath.length>0){
-       result.saveAsObjectFile(outputPath)
+       result.saveAsTextFile(outputPath)
     }else{
-      result.foreach(e=>println(e._2))
+      result.foreach(e=>println(e))
     }
   }
-  def addLDAFeature(doc:CompositeDoc,vector:org.apache.spark.mllib.linalg.Vector): Unit ={
-
-    val list = new java.util.ArrayList()
+  def addLDAFeature(doc:CompositeDoc,vector:Array[scala.Double]): Unit ={
+    doc.media_doc_info.setLdavec(new util.ArrayList[Double]())
     for(s<-vector){
-      list.add(s)
+       doc.media_doc_info.ldavec.add(s)
     }
-    doc.media_doc_info.setLdavec(list.toList)
   }
 
-  def mergeLDAFeature(rddBase64:RDD[(String,String)], outputPath:String): Unit ={
-    val sc=rddBase64.sparkContext
-    val composticDoc=rddBase64
-      .map(e=>DocProcess.CompositeDocSerialize.DeSerialize(e._2, null)).map((_,1))
-    val feature=sc.objectFile[(CompositeDoc,org.apache.spark.mllib.linalg.Vector)](outputPath)
-    composticDoc.leftOuterJoin(feature).map{e=>
-    val doc=e._1
-    val f=e._2._2
-    f match {
-      case Some(vector)=>addLDAFeature(doc,vector)
-      case _=>
+  def mergeLDAFeature(rddBase64:RDD[(String,String)], outputPath:String): RDD[CompositeDoc] = {
+    val sc = rddBase64.sparkContext
+    val rdd = rddBase64
+      .map(e => DocProcess.CompositeDocSerialize.DeSerialize(e._2, null))
+      .map(e => (e.media_doc_info.id, e))
+    val feature = sc.textFile(outputPath)
+      .map(e => e.split("\t")).map(e => (e(0), e(1).split(" ").map(ee => ee.toDouble)))
+    //rdd.foreach(e => println(e._1))
+    val result=rdd.leftOuterJoin(feature).map{ e =>
+      val doc = e._2._1
+      val f = e._2._2
+      f match {
+        case Some(v)=>addLDAFeature(doc,v)
+        case _=> doc.media_doc_info.setLdavec(new util.ArrayList[Double]())
+      }
+      doc
     }
-
+    result
   }
-  }
-
   def main(args: Array[String]): Unit = {
     var masterUrl = "local[2]"
     val sparkConf = new SparkConf()
     val sc = new SparkContext(masterUrl, "SparkHBaseDBExtraction", sparkConf)
-    val rddBase64=sc.textFile("C:\\Users\\lujing1\\Desktop\\LabelTag\\hdfs_data_input")
+    val rddBase64=sc.
+      textFile("C:\\Users\\lujing1\\Desktop\\LabelTag\\hdfs_data_input")
       .map(e=>e.split("\t")).map(e=>(e(0),e(1)))
-    calLDAFeature(rddBase64,"")
+    //calLDAFeature(rddBase64,"D:\\a")
+    val result=mergeLDAFeature(rddBase64,"D:\\a")
+    result.foreach(e=>println(e.media_doc_info.getLdavec.mkString(" ")))
+
   }
 
 
